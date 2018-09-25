d8f80816b6c78f8a1fdead6498d86dfdeed7d35d
Eric0812 <eric.araujo@edu.se.df.gov.br>
2018-09-25 14:24:06 -0300

    segundo commit

--- app/controllers/shopping_controller.rb
+++ app/controllers/shopping_controller.rb
@@ -1,8 +1,15 @@
 class ShoppingController < ApplicationController
   def new
+    @shop = Shop.new
   end
 
   def create
+    shop = Shop.new(permit)
+    if shop.save
+      @msg = 'salvo com sucesso'
+    else
+      @msg = shop.errors.messages
+    end
   end
 
   def edit
@@ -12,5 +19,11 @@ class ShoppingController < ApplicationController
   end
 
   def destroy
+  end
+
+private
+
+  def permit 
+    params['shop'].permit(:name, :address)
   end
 end
